ee9e44381926b383b651f68b9c0419668fd0940e
unknown <vikrant.sharma1>
2020-09-29 17:19:26 +0530

    second commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,34 @@
-import React from 'react';
+
+import React, { useEffect } from 'react';
 import LaunchProgramsContainer from '../components/LaunchPrograms/LaunchProgramsContainer';
+import { Provider } from 'react-redux';
+import store from '../../store';
+import Head from 'next/head';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import {ThemeProvider } from '@material-ui/core/styles';
+import theme from '../../theme';
 
 function App() {
-  return (
-    <LaunchProgramsContainer/>
+  useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+  return (<React.Fragment>
+    <Head>
+      <title>SpaceX</title>
+      <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
+    </Head>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Provider store={store}>
+        <LaunchProgramsContainer />
+      </Provider>
+    </ThemeProvider>
+  </React.Fragment>
   );
 }
 
